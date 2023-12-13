1ea63428ab6b9cde784e2662a418ddbe2ee50475
Kalilou Marong <95428439+kalilou-marong@users.noreply.github.com>
2023-12-13 11:59:06 +0100

    Insertion des données dans la table realisateurs

--- movies.sql
+++ movies.sql
@@ -8,4 +8,14 @@ CREATE TABLE realisateurs (id INT PRIMARY KEY NOT NULL, nom VARCHAR(100) NOT Nul
 //Création des clés étrangères
 ALTER TABLE films ADD CONSTRAINT fk_realisateur FOREIGN KEY (id) REFERENCES realisateurs(id);
 ALTER TABLE films ADD CONSTRAINT fk_acteurs FOREIGN KEY (id) REFERENCES acteurs(id);
-
+//Insertion des données dans la table réalisateurs
+INSERT INTO realisateurs (id, nom, prenom) VALUES
+    (1, 'Nolan', 'Christopher'),
+    (2, 'Zemeckis', 'Robert'),
+    (3, 'Tarantino', 'Quentin'),
+    (4, 'Darabont', 'Frank'),
+    (5, 'Wachowski', 'Lana'),
+    (6, 'Cameron', 'James'),
+    (7, 'Spielberg', 'Steven'),
+    (8, 'Coppola', 'Francis Ford'),
+    (9, 'Cameron', 'James')
