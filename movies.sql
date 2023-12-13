44031c03fd4f67e096c247fbd86b538c7f03f736
Kalilou Marong <95428439+kalilou-marong@users.noreply.github.com>
2023-12-13 11:01:12 +0100

    Update movies.sql

--- movies.sql
+++ movies.sql
@@ -1,1 +1,8 @@
+//Création de la base de données
 CREATE movies;
+//Création des tables
+CREATE TABLE utilisateurs (id INT PRIMARY KEY NOT NULL, email VARCHAR(100) NOT Null, password VARCHAR(100) NOT NULL);
+CREATE TABLE films (id INT PRIMARY KEY NOT NULL, titre VARCHAR(100) NOT Null, durée TIMESTAMP NOT NULL, sortie DATETIME NOT NULL);
+CREATE TABLE acteurs (id INT PRIMARY KEY NOT NULL, nom VARCHAR(100) NOT Null, prenom VARCHAR(100) NOT NULL, date_de_naissance DATETIME NOT NULL);
+CREATE TABLE realisateurs (id INT PRIMARY KEY NOT NULL, nom VARCHAR(100) NOT Null, prenom VARCHAR(100) NOT NULL);
+
